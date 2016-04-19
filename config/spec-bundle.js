1c9e9a525c5e968f90e76704825d32e00995e373
Artem Govorov <artem.govorov@gmail.com>
2016-04-19 17:56:41 +1000

    Update spec-bundle.js

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -33,23 +33,5 @@ testing.setBaseTestProviders(
   browser.TEST_BROWSER_APPLICATION_PROVIDERS);
 
 Object.assign(global, testing);
-/*
-  Ok, this is kinda crazy. We can use the the context method on
-  require that webpack created in order to tell webpack
-  what files we actually want to require or import.
-  Below, context will be an function/object with file names as keys.
-  using that regex we are saying look in ./src/app and ./test then find
-  any file that ends with spec.js and get its path. By passing in true
-  we say do this recursively
-*/
-var testContext = require.context('../src', true, /\.spec\./);
 
-// get all the files, for each file, call the context function
-// that will require the file and load it up here. Context will
-// loop and require those spec files here
-function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-}
-
-var modules = requireAll(testContext);
-// requires and returns all modules that match
+window.__karma__ && require('./karma-require');
